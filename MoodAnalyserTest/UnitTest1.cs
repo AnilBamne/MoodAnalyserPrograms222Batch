c02fd3b5192abeb4d663da145de39ab10795f91a
AnilBamne <anilbamne20@gmail.com>
2022-12-06 22:13:25 +0530

    Adding : UC1CheckMoodHappyOrSad code

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace MoodAnalyserTest
         /// Test method
         /// when given sad mood , retunr SAD
         /// </summary>
-        [TestMethod]
+        [TestMethod]                                            //TC1.1
         public void Given_WhenSadMood_ThenReturn_SAD()
         {
             //AAA- Arrange-Act-Assert
@@ -23,6 +23,20 @@ namespace MoodAnalyserTest
             string expected = "SAD";
             //Act
             string actual=moodAnalyser.AnalyseMood(message);
+            //Assert
+            //Using Assert class for testing
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]                                            //TC1.2
+        public void Given_WhenAnyMood_ThenReturn_HAPPY()
+        {
+            //AAA- Arrange-Act-Assert
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            //Arrange
+            string message = "Iam in Any mood";
+            string expected = "HAPPY";
+            //Act
+            string actual = moodAnalyser.AnalyseMood(message);
             //Assert
             //Using Assert class for testing
             Assert.AreEqual(expected, actual);

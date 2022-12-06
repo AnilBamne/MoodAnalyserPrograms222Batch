bf166b48818093e60558e566a15eb9556902a654
AnilBamne <anilbamne20@gmail.com>
2022-12-06 19:43:33 +0530

    Adding : welcome message

---
+++ MoodAnalyser222Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyser222Batch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+        }
+    }
+}
